c436fc7500265ca68458ed14762b96187d6e5b0c
Travis <tgpovey@gmail.com>
2017-05-20 19:14:43 +0800

    AnalyticS!

---
+++ Assets/Tracker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tracker : MonoBehaviour {
+
+    public GoogleAnalyticsV4 googleAnalytics;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
